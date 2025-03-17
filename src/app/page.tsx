a032968cfb1cac9c5065413c90883923067380e0
fengminhua <fengminhua@52tt.com>
2025-03-17 23:46:01 +0800

    feat:init

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold">hello miwa</h1>
+    </div>
+  );
+}
